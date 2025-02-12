c8d44d926836e101f573d42a705e4cc98d44889b
Siwei <cmonzoey22@gmail.com>
2025-02-12 11:02:47 +0100

    rename tome to tomesd, import the actual tome

--- tome_sam/tome_algo/tome/merge.py
+++ tome_sam/tome_algo/tome/merge.py
@@ -1,7 +1,15 @@
-# Adapt from tomesd.merge (https://github.com/dbolya/tomesd/blob/main/tomesd/merge.py)
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+
+import math
+from typing import Callable, Tuple
 
 import torch
-from typing import Tuple, Callable
+
 
 def safe_normalize(x: torch.Tensor, dim: int=-1, eps: float=1e-12):
     """
@@ -34,123 +42,239 @@ def mps_gather_workaround(input, dim, index):
         ).squeeze(-1)
     else:
         return torch.gather(input, dim, index)
-
-
-def bipartite_soft_matching_random2d(metric: torch.Tensor,
-                                     w: int, h: int, sx: int, sy: int, r: int,
-                                     no_rand: bool = False,
-                                     generator: torch.Generator = None) -> Tuple[Callable, Callable]:
+def bipartite_soft_matching(
+    metric: torch.Tensor,
+    r: int,
+    class_token: bool = False,
+    distill_token: bool = False,
+) -> Tuple[Callable, Callable]:
     """
-    Partitions the tokens into src and dst and merges r tokens from src to dst.
-    Dst tokens are partitioned by choosing one randomy in each (sx, sy) region.
+    Applies ToMe with a balanced matching set (50%, 50%).
 
-    Args:
-     - metric [B, N, C]: metric to use for similarity
-     - w: image width in tokens
-     - h: image height in tokens
-     - sx: stride in the x dimension for dst, must divide w
-     - sy: stride in the y dimension for dst, must divide h
-     - r: number of tokens to remove (by merging)
-     - no_rand: if true, disable randomness (use top left corner only)
-     - rand_seed: if no_rand is false, and if not None, sets random seed.
+    Input size is [batch, tokens, channels].
+    r indicates the number of tokens to remove (max 50% of tokens).
+
+    Extra args:
+     - class_token: Whether or not there's a class token.
+     - distill_token: Whether or not there's also a distillation token.
+
+    When enabled, the class token and distillation tokens won't get merged.
     """
-    B, N, _ = metric.shape
+    gather = mps_gather_workaround if metric.device.type == "mps" else torch.gather
+
+    protected = 0
+    if class_token:
+        protected += 1
+    if distill_token:
+        protected += 1
+
+    # We can only reduce by a maximum of 50% tokens
+    t = metric.shape[1]
+    r = min(r, (t - protected) // 2)
 
     if r <= 0:
         return do_nothing, do_nothing
 
-    gather = mps_gather_workaround if metric.device.type == "mps" else torch.gather
-
     with torch.no_grad():
-        hsy, wsx = h // sy, w // sx
-
-        # For each sy by sx kernel, randomly assign one token to be dst and the rest src
-        if no_rand:
-            rand_idx = torch.zeros(hsy, wsx, 1, device=metric.device, dtype=torch.int64)
-        else:
-            rand_idx = torch.randint(sy * sx, size=(hsy, wsx, 1), device=generator.device, generator=generator).to(
-                metric.device)
-
-        # The image might not divide sx and sy, so we need to work on a view of the top left if the idx buffer instead
-        idx_buffer_view = torch.zeros(hsy, wsx, sy * sx, device=metric.device, dtype=torch.int64)
-        idx_buffer_view.scatter_(dim=2, index=rand_idx, src=-torch.ones_like(rand_idx, dtype=rand_idx.dtype))
-        idx_buffer_view = idx_buffer_view.view(hsy, wsx, sy, sx).transpose(1, 2).reshape(hsy * sy, wsx * sx)
-
-        # Image is not divisible by sx or sy so we need to move it into a new buffer
-        if (hsy * sy) < h or (wsx * sx) < w:
-            idx_buffer = torch.zeros(h, w, device=metric.device, dtype=torch.int64)
-            idx_buffer[:(hsy * sy), :(wsx * sx)] = idx_buffer_view
-        else:
-            idx_buffer = idx_buffer_view
-
-        # We set dst tokens to be -1 and src to be 0, so an argsort gives us dst|src indices
-        rand_idx = idx_buffer.reshape(1, -1, 1).argsort(dim=1)
-
-        # We're finished with these
-        del idx_buffer, idx_buffer_view
-
-        # rand_idx is currently dst|src, so split them
-        num_dst = hsy * wsx
-        a_idx = rand_idx[:, num_dst:, :]  # src
-        b_idx = rand_idx[:, :num_dst, :]  # dst
-
-        def split(x):
-            C = x.shape[-1]
-            src = gather(x, dim=1, index=a_idx.expand(B, N - num_dst, C))
-            dst = gather(x, dim=1, index=b_idx.expand(B, num_dst, C))
-            return src, dst
-
-        # Cosine similarity between A and B
-        metric = safe_normalize(metric, dim=-1)
-        a, b = split(metric)
+        metric = metric / metric.norm(dim=-1, keepdim=True)
+        a, b = metric[..., ::2, :], metric[..., 1::2, :]
         scores = a @ b.transpose(-1, -2)
 
-        # Can't reduce more than the # tokens in src
-        r = min(a.shape[1], r)
+        indices = torch.arange(t).to(metric.device)
+        # Select even and odd indices
+        a_idx = indices[::2].unsqueeze(0).unsqueeze(-1)  # (1, N/2, 1)
+        b_idx = indices[1::2].unsqueeze(0).unsqueeze(-1)  # (1, N/2, 1)
 
-        # Find the most similar greedily
+        if class_token:
+            scores[..., 0, :] = -math.inf
+        if distill_token:
+            scores[..., :, 0] = -math.inf
+
         node_max, node_idx = scores.max(dim=-1)
         edge_idx = node_max.argsort(dim=-1, descending=True)[..., None]
 
         unm_idx = edge_idx[..., r:, :]  # Unmerged Tokens
         src_idx = edge_idx[..., :r, :]  # Merged Tokens
-        dst_idx = gather(node_idx[..., None], dim=-2, index=src_idx)
+        dst_idx = node_idx[..., None].gather(dim=-2, index=src_idx)
 
+        if class_token:
+            # Sort to ensure the class token is at the start
+            unm_idx = unm_idx.sort(dim=1)[0]
 
     def merge(x: torch.Tensor, mode="mean") -> Tuple[torch.Tensor, torch.Tensor]:
-        src, dst = split(x)
+        src, dst = x[..., ::2, :], x[..., 1::2, :]
         n, t1, c = src.shape
-
         unm = gather(src, dim=-2, index=unm_idx.expand(n, t1 - r, c))
         src = gather(src, dim=-2, index=src_idx.expand(n, r, c))
         dst = dst.scatter_reduce(-2, dst_idx.expand(n, r, c), src, reduce=mode)
-        merged_tokens = torch.cat([unm, dst], dim=1)
 
         # To find out indices w.r.t input tensor x, above unm_idx and src_idx are w.r.t src(a_idx), dst_idx is w.r.t dst(b_idx)
         # (B*num_heads, N_unm)
-        unm_absolute_indices = gather(a_idx.expand(n, a_idx.shape[1], 1), dim=1, index=unm_idx).squeeze(-1)
+        unm_absolute_indices = gather(a_idx.expand(n, a.shape[1], 1), dim=1, index=unm_idx).squeeze(-1)
         # (B*num_heads, N_dst)
         dst_absolute_indices = b_idx.squeeze(-1).expand(n, -1)
         absolute_indices = torch.cat([unm_absolute_indices, dst_absolute_indices], dim=1)
 
-        return merged_tokens, absolute_indices
-
+        if distill_token:
+            raise NotImplementedError
+        else:
+            return torch.cat([unm, dst], dim=1), absolute_indices
 
     def unmerge(x: torch.Tensor) -> torch.Tensor:
-        _, _, c = x.shape
         unm_len = unm_idx.shape[1]
         unm, dst = x[..., :unm_len, :], x[..., unm_len:, :]
+        n, _, c = unm.shape
 
-        src = gather(dst, dim=-2, index=dst_idx.expand(B, r, c))
-        # Combine back to the original shape
-        out = torch.zeros(B, N, c, device=x.device, dtype=x.dtype)
-        out.scatter_(dim=-2, index=b_idx.expand(B, num_dst, c), src=dst)
-        out.scatter_(dim=-2,
-                     index=gather(a_idx.expand(B, a_idx.shape[1], 1), dim=1, index=unm_idx).expand(B, unm_len, c),
-                     src=unm)
-        out.scatter_(dim=-2, index=gather(a_idx.expand(B, a_idx.shape[1], 1), dim=1, index=src_idx).expand(B, r, c),
-                     src=src)
+        src = dst.gather(dim=-2, index=dst_idx.expand(n, r, c))
+
+        out = torch.zeros(n, metric.shape[1], c, device=x.device, dtype=x.dtype)
+
+        out[..., 1::2, :] = dst
+        out.scatter_(dim=-2, index=(2 * unm_idx).expand(n, unm_len, c), src=unm)
+        out.scatter_(dim=-2, index=(2 * src_idx).expand(n, r, c), src=src)
 
         return out
 
     return merge, unmerge
+
+
+def kth_bipartite_soft_matching(
+    metric: torch.Tensor, k: int
+) -> Tuple[Callable, Callable]:
+    """
+    Applies ToMe with the two sets as (every kth element, the rest).
+    If n is the number of tokens, resulting number of tokens will be n // z.
+
+    Input size is [batch, tokens, channels].
+    z indicates the stride for the first set.
+    z = 2 is equivalent to regular bipartite_soft_matching with r = 0.5 * N
+    """
+    if k <= 1:
+        return do_nothing, do_nothing
+
+    def split(x):
+        t_rnd = (x.shape[1] // k) * k
+        x = x[:, :t_rnd, :].view(x.shape[0], -1, k, x.shape[2])
+        a, b = (
+            x[:, :, : (k - 1), :].contiguous().view(x.shape[0], -1, x.shape[-1]),
+            x[:, :, (k - 1), :],
+        )
+        return a, b
+
+    with torch.no_grad():
+        metric = metric / metric.norm(dim=-1, keepdim=True)
+        a, b = split(metric)
+        r = a.shape[1]
+        scores = a @ b.transpose(-1, -2)
+
+        _, dst_idx = scores.max(dim=-1)
+        dst_idx = dst_idx[..., None]
+
+    def merge(x: torch.Tensor, mode="mean") -> torch.Tensor:
+        src, dst = split(x)
+        n, _, c = src.shape
+        dst = dst.scatter_reduce(-2, dst_idx.expand(n, r, c), src, reduce=mode)
+
+        return dst
+
+    def unmerge(x: torch.Tensor) -> torch.Tensor:
+        n, _, c = x.shape
+        dst = x
+
+        src = dst.gather(dim=-2, index=dst_idx.expand(n, r, c)).to(x.dtype)
+
+        src = src.view(n, -1, (k - 1), c)
+        dst = dst.view(n, -1, 1, c)
+
+        out = torch.cat([src, dst], dim=-2)
+        out = out.contiguous().view(n, -1, c)
+
+        return out
+
+    return merge, unmerge
+
+
+def random_bipartite_soft_matching(
+    metric: torch.Tensor, r: int
+) -> Tuple[Callable, Callable]:
+    """
+    Applies ToMe with the two sets as (r chosen randomly, the rest).
+    Input size is [batch, tokens, channels].
+
+    This will reduce the number of tokens by r.
+    """
+    if r <= 0:
+        return do_nothing, do_nothing
+
+    with torch.no_grad():
+        B, N, _ = metric.shape
+        rand_idx = torch.rand(B, N, 1, device=metric.device).argsort(dim=1)
+
+        a_idx = rand_idx[:, :r, :]
+        b_idx = rand_idx[:, r:, :]
+
+        def split(x):
+            C = x.shape[-1]
+            a = x.gather(dim=1, index=a_idx.expand(B, r, C))
+            b = x.gather(dim=1, index=b_idx.expand(B, N - r, C))
+            return a, b
+
+        metric = metric / metric.norm(dim=-1, keepdim=True)
+        a, b = split(metric)
+        scores = a @ b.transpose(-1, -2)
+
+        _, dst_idx = scores.max(dim=-1)
+        dst_idx = dst_idx[..., None]
+
+    def merge(x: torch.Tensor, mode="mean") -> torch.Tensor:
+        src, dst = split(x)
+        C = src.shape[-1]
+        dst = dst.scatter_reduce(-2, dst_idx.expand(B, r, C), src, reduce=mode)
+
+        return dst
+
+    def unmerge(x: torch.Tensor) -> torch.Tensor:
+        C = x.shape[-1]
+        dst = x
+        src = dst.gather(dim=-2, index=dst_idx.expand(B, r, C))
+
+        out = torch.zeros(B, N, C, device=x.device, dtype=x.dtype)
+
+        out.scatter_(dim=-2, index=a_idx.expand(B, r, C), src=src)
+        out.scatter_(dim=-2, index=b_idx.expand(B, N - r, C), src=dst)
+
+        return out
+
+    return merge, unmerge
+
+
+def merge_wavg(
+    merge: Callable, x: torch.Tensor, size: torch.Tensor = None
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Applies the merge function by taking a weighted average based on token size.
+    Returns the merged tensor and the new token sizes.
+    """
+    if size is None:
+        size = torch.ones_like(x[..., 0, None])
+
+    x = merge(x * size, mode="sum")
+    size = merge(size, mode="sum")
+
+    x = x / size
+    return x, size
+
+
+def merge_source(
+    merge: Callable, x: torch.Tensor, source: torch.Tensor = None
+) -> torch.Tensor:
+    """
+    For source tracking. Source is an adjacency matrix between the initial tokens and final merged groups.
+    x is used to find out how many tokens there are in case the source is None.
+    """
+    if source is None:
+        n, t, _ = x.shape
+        source = torch.eye(t, device=x.device)[None, ...].expand(n, t, t)
+
+    source = merge(source, mode="amax")
+    return source

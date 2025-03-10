eb02ce9a3c7e2b10ecb999e7aa487e8f67523bec
Siwei <cmonzoey22@gmail.com>
2025-03-10 10:42:23 +0100

    remove unnecessary comments

--- tome_sam/tome_algo/grad_tome/grad_tome.py
+++ tome_sam/tome_algo/grad_tome/grad_tome.py
@@ -1,14 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-# --------------------------------------------------------
-
-# Adapt from https://github.com/hchautran/PiToMe/blob/main/algo/pitome/merge.py, remove class token, add unmerge part because
-# our task is segmentation.
-
-import math
 from typing import Callable, Tuple
 import torch
 import torch.nn.functional as F
